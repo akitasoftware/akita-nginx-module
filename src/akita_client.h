594ce85eba3361475938f14532b033775404218a
Mark Gritter <mgritter@akitasoftware.com>
2023-01-12 02:46:04 -0600

    Send request metadata to Akita agent.  (#3)
    Mini-library for writing JSON.
    Updated proxy instructions.
    
    Co-authored-by: Jed Liu <liujed@users.noreply.github.com>

---
+++ src/akita_client.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023 Akita Software
+ */
+
+#ifndef _AKITA_NGX_MODULE_AKITA_CLIENT_H_INCLUDED
+#define _AKITA_NGX_MODULE_AKITA_CLIENT_H_INCLUDED
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+/* Top-level functions for sending data to the Akita agent. */
+
+/* Initialize the Akita client based on the current configuration. */
+ngx_int_t
+ngx_akita_client_init(ngx_conf_t *cf);
+                      
+/*
+ * Send a REST call (as a subrequest) reporting on an HTTP request body.
+ * Takes the original request and (for now) a path that will proxy to the
+ * client. This should be called in a body callback where the entire request
+ * body is already available.
+ *
+ * TODO: replace agent_path with an Nginx upstream.
+ */
+ngx_int_t
+ngx_akita_send_request_body(ngx_http_request_t *r, ngx_str_t agent_path,
+                            ngx_http_post_subrequest_t *callback);
+
+#endif /* NGX_AKITA_MODULE_AKITA_CLIENT_H_INCLUDED */
